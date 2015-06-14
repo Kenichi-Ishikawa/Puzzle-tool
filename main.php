2c93c30cd9449fe3e2d0c8d5998efd92831efe70
Kenichi-Ishikawa <ff11noisy@gmail.com>
2015-06-14 12:56:09 +0900

    バグってるけど戻しとく

--- main.php
+++ main.php
@@ -110,7 +110,9 @@ function BlockCheck($answer, $maru, $batu) {
 		list ($answer, $maru, $batu) = BlockCheckDetail($answer, $maru, $batu, 0, 1, 0, 0, 3, 0);
 		list ($answer, $maru, $batu) = BlockCheckDetail($answer, $maru, $batu, 0, 1, 3, 0, 0, 0);
 		list ($answer, $maru, $batu) = BlockCheckDetail($answer, $maru, $batu, 0, 2, 0, 0, 2, 0);
+		list ($answer, $maru, $batu) = BlockCheckDetail($answer, $maru, $batu, 0, 2, 2, 0, 0, 0);
 		list ($answer, $maru, $batu) = BlockCheckDetail($answer, $maru, $batu, 0, 3, 0, 0, 1, 0);
+		list ($answer, $maru, $batu) = BlockCheckDetail($answer, $maru, $batu, 0, 3, 1, 0, 0, 0);
 		list ($answer, $maru, $batu) = BlockCheckDetail($answer, $maru, $batu, 0, 3, 1, 1, 0, 1);
 		list ($answer, $maru, $batu) = BlockCheckDetail($answer, $maru, $batu, 1, 2, 2, 1, 1, 1);
 
